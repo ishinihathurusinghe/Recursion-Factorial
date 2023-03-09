ef3f453ffe8c4209c4a0014fb5e5c0ae05c64136
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2023-03-09 14:45:35 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,4 +1,4 @@
-                                        public class Factorial {
+                                         public class Factorial {
     public static int factorial(int n) {
         if (n == 0) {
             return 1;
